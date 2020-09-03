fabb7fec493cf948beed1f5e44a0daea7e3f5c79
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-09-03 08:54:12 +0200

    added actors for execution cases

--- src/main/scala/org/xcasemanager/datacollector/Main.scala
+++ src/main/scala/org/xcasemanager/datacollector/Main.scala
@@ -1,15 +1,14 @@
 package org.xcasemanager.datacollector
 
 import akka.actor.{ActorSystem, Props}
+import org.xcasemanager.datacollector.actors.project.ExecutionDataProcessActor
+import org.xcasemanager.datacollector.actors.project.ExecutionRepoActor
 
 object Main extends App {
-
     val actorSystem = ActorSystem.create("collector")
     val httpActor = actorSystem.actorOf(Props[HttpActor],"httpActor")
-    val caseExecutionRegisterActor = actorSystem.actorOf(Props[CaseExecutionRegisterActor], "caseExecutionRegisterActor")
-    val executionReportMappingActor = actorSystem.actorOf(Props[ExecutionReportMappingActor], "executionReportMappingActor")
-    val reportActor = actorSystem.actorOf(Props[ReportActor], "reportActor")
     val logActor = actorSystem.actorOf(Props[LogActor], "logActor")
-
+    val executionDataProcessActor = actorSystem.actorOf(Props[ExecutionDataProcessActor], "executionDataProcessActor")
+    val executionRepoActor = actorSystem.actorOf(Props[ExecutionRepoActor], "executionRepoActor")
     httpActor ! StartWebServerCommand
 }
