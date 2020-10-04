a9183a50f77b0d4041289d985f7fc7479b64bed9
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-10-04 15:12:21 +0200

    removed obsolete packages

--- src/main/scala/org/xcasemanager/datacollector/Main.scala
+++ src/main/scala/org/xcasemanager/datacollector/Main.scala
@@ -1,13 +1,11 @@
 package org.xcasemanager.datacollector
 
 import akka.actor.{ActorSystem, Props}
-import org.xcasemanager.datacollector.actors.project.ExecutionDataProcessActor
 import org.xcasemanager.datacollector.actors.project.ExecutionRepoActor
 
 object Main extends App {
     val actorSystem = ActorSystem.create("collector")
     val httpActor = actorSystem.actorOf(Props[HttpActor],"httpActor")
-    val executionDataProcessActor = actorSystem.actorOf(Props[ExecutionDataProcessActor], "executionDataProcessActor")
     val executionRepoActor = actorSystem.actorOf(Props[ExecutionRepoActor], "executionRepoActor")
     httpActor ! StartWebServerCommand
 }
