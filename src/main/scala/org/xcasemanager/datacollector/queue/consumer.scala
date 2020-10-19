9e19faf368b567b2cc42dced38c2453bf751fec3
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-10-19 10:13:12 +0200

    set consummer sink

--- src/main/scala/org/xcasemanager/datacollector/queue/consumer.scala
+++ src/main/scala/org/xcasemanager/datacollector/queue/consumer.scala
@@ -2,12 +2,14 @@ package org.xcasemanager.datacollector.queue
 
 import akka.kafka.scaladsl.Consumer
 import akka.kafka.{ConsumerSettings, Subscriptions}
+import akka.stream.scaladsl.Sink
 import akka.actor.{Actor, ActorLogging}
 import scala.concurrent.Future
 import scala.language.postfixOps
-import org.apache.kafka.common.serialization.{StringSerializer, ByteArraySerializer}
+import org.apache.kafka.common.serialization._
 import org.xcasemanager.datacollector.queue.command.StartQueueConsumerCommand
 import org.xcasemanager.datacollector.message._
+
 
 /**
 * Queue Consumer
@@ -15,7 +17,10 @@ import org.xcasemanager.datacollector.message._
 class Consumer extends Actor with JsonSupport with ActorLogging {
 
   implicit val system = context.system
+
   val config = system.settings.config.getConfig("akka.kafka.consumer")
+  val consumerSettings =
+    ConsumerSettings(config, new StringDeserializer, new ByteArrayDeserializer)
   val topicName = system.settings.config.getString("queue.topic.name")
  
   /*
@@ -31,7 +36,7 @@ class Consumer extends Actor with JsonSupport with ActorLogging {
     listen to topic
   */
   def startConsumer = {
-    Consumer.plainSource(config, Subscriptions.topics(topicName))
+    Consumer.plainSource(consumerSettings, Subscriptions.topics(topicName))
       .mapAsync(1) ( msg => {
         log.info(s"Message Received : ${msg.timestamp} - ${msg.value}")
       Future.successful(msg)
